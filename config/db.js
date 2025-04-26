56c8aa998add5e9f1b04343ac50717d9c50f71dc
Chinmay <chinmaybadwaik9@gmail.com>
2025-04-26 12:47:18 +0530

    Updated db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
-    console.log('MongoDB Connection Failed');
+    console.log('MongoDB Connection Failed',error);
     process.exit(1);
   }
 };
